0b8b5553a516b302cdcc074b6515410aff03152d
iceblue47 <88323298+iceblue47@users.noreply.github.com>
2023-10-21 12:56:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 REPO1="https://raw.githubusercontent.com/myridwan/abc/ipuk/"
 REPO="https://raw.githubusercontent.com/myridwan/scriptvps/ipuk/"
-CDNF="https://raw.githubusercontent.com/myridwan/vvip/ipuk"
+CDNF="https://raw.githubusercontent.com/iceblue47/yeniwsssh"
 ###
 BURIQ () {
     curl -sS https://raw.githubusercontent.com/iceblue47/bca/main/ip > /root/tmp
